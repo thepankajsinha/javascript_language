f2e2fc46865dd62ffb40ff85a4dac60e4235253f
Pankaj Sinha <136917326+thepankajsinha@users.noreply.github.com>
2024-10-02 11:30:16 +0530

    Delete Array directory

--- Array/MapMethod.js
+++
@@ -1,9 +0,0 @@
-//Purpose: Transforms each element in an array according to a specified function.
-
-//Returns: A new array containing the results of applying the function to each element.
-
-const numbers = [1, 2, 3, 4, 5];
-const squared = numbers.map(num => num * num);
-
-console.log(squared);
-//[1, 4, 9, 16, 25]
